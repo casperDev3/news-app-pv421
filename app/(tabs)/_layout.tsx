8116b14510c8beb15e69c4ae08dbc5b6cfb42f6a
Ihor Lialiuk <93039953+casperDev3@users.noreply.github.com>
2026-04-18 12:53:09 +0300

    Add tab icons and color constants
    Introduce color constants and add focused/unfocused tab icons. Created constants/colors.tsx (Primary and Grayscale palettes), updated app/(tabs)/_layout.tsx to import lucide-react-native icons and use tabBarIcon renderers with Primary.default for active and Grayscale.bodyText for inactive states, and replaced the splash-icon.png asset.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,9 +1,12 @@
 import {Tabs} from 'expo-router';
 import React from 'react';
+import {Home, Compass, Bookmark, CircleUserRound} from "lucide-react-native"
+import {Grayscale, Primary} from "@/constants/colors";
 
 export default function TabLayout() {
     return (
         <Tabs
+
             screenOptions={{
                 headerShown: false,
             }}>
@@ -11,24 +14,42 @@ export default function TabLayout() {
                 name="index"
                 options={{
                     title: 'Home',
+                    tabBarIcon: ({focused}) => (
+                        focused ? <Home size={24} color={Primary.default}/> :
+                            <Home size={24} color={Grayscale.bodyText}/>
+                    ),
                 }}
             />
             <Tabs.Screen
                 name="explore"
                 options={{
                     title: 'Explore',
+                    tabBarIcon: ({focused}) => (
+                        focused ? <Compass size={24} color={Primary.default}/> :
+                            <Compass size={24} color={Grayscale.bodyText}/>
+                    )
                 }}
             />
             <Tabs.Screen
                 name="bookmarks"
                 options={{
                     title: 'Bookmarks',
+                    tabBarIcon: ({focused}) => (
+                        focused ? <Bookmark  size={24} color={Primary.default}/> :
+                            <Bookmark size={24} color={Grayscale.bodyText}/>
+                    )
+
                 }}
             />
             <Tabs.Screen
                 name="profile"
                 options={{
                     title: 'Profile',
+                    tabBarIcon: ({focused}) => (
+                        focused ? <CircleUserRound size={24} color={Primary.default}/> :
+                            <CircleUserRound size={24} color={Grayscale.bodyText}/>
+                    ),
+
                 }}
             />
         </Tabs>
---
+++ constants/colors.tsx
@@ -0,0 +1,8 @@
+export const Grayscale = {
+    textActive: "#050505",
+    bodyText: "#4E4B66",
+}
+
+export const Primary = {
+    default: "#1877F2",
+}
